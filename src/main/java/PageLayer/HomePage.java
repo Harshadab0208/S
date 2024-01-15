4fe7174787e0492e6df83ae25516a46d6aab894c
admin <totareg@gmail.com>
2024-01-14 22:30:04 -0800

    home page created

---
+++ src/main/java/PageLayer/HomePage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+import BaseLayer.BaseClass;
+
+public class HomePage extends BaseClass {
+	
+	public void sendKeyss() {
+		System.out.println("home page code is added");
+	}
+
+}
